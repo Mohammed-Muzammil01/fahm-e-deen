b5bb891aa827d809b4f43f3ab5f30b00d7f5c464
Mohammed-Muzammil01 <muzzzz2k3@gmail.com>
2024-04-05 00:43:37 +0300

    added new events

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,24 +1,31 @@
 import React from 'react'
 import post6 from "../assets/post6.jpeg"
-import post7 from "../assets/post7.jpeg"
-import post8 from "../assets/post8.jpeg"
+import post7 from "../assets/post7.jpg"
+import post8 from "../assets/post8.jpg"
 import post3 from "../assets/post3.jpeg"
 import post4 from "../assets/post4.jpeg"
 import post1 from "../assets/post1.jpeg"
-import post2 from "../assets/post2.jpeg"
+import post5 from "../assets/post5.jpg"
+import post9 from "../assets/post9.jpg"
+import post10 from "../assets/post10.jpg"
+import post11 from "../assets/post11.jpg"
+
 
 
 function Events() {
     return (
         <>
             <h2>News & Events</h2>
-            <img alt="" className="post" src={post6} />
+            <img alt="" className="post" src={post11} />
+            <img alt="" className="post" src={post5} />
             <img alt="" className="post" src={post7} />
+            <img alt="" className="post" src={post9} />
             <img alt="" className="post" src={post8} />
+            <img alt="" className="post" src={post10} />
+            <img alt="" className="post" src={post6} />
             <a href="https://forms.gle/LJRR9L5ymhMNnL1W8" target="_blank" rel='noreferrer'><img alt="" className="post" src={post4} /></a>
             <a href="https://docs.google.com/forms/d/e/1FAIpQLSdRiu7H6ADfT3i-XlN5xnx8KoEHHr03abui2p0izb0G8SV3iw/viewform" target="_blank" rel='noreferrer'><img alt="" className="post" src={post3} /></a>
             <img alt="" className="post" src={post1} />
-            <img alt="" className="post" src={post2} />
         </>
     )
 }
--- src/pages/Teachers.jsx
+++ src/pages/Teachers.jsx
@@ -10,38 +10,59 @@ function Teachers() {
 				<div className="teacher">
 				<h2>Ustadha Touqeer Nishath</h2>
 				<p>
-					Ustadha Touqeer is born and raised in India and completed Masters in Commerce from Osmania University. She has graduated in Alimah programme from Nur ul ilm, Dubai. Presently learning and sharing the knowledge of Quran with the sisters only by the mercy of Allah (s.w.t)
+				Touqeer Nishath is born and raised in India and completed Masters in Commerce from Osmania University. She has graduated in Alimah programme from Nur ul ilm, Dubai. Presently learning and sharing the knowledge of Quran with the sisters only by the mercy of Allah (s.w.t)
 				</p>
 				</div>
 				<div className="teacher">
 				<h2>Ustadha Qizra Habeeb</h2>
 				<p>
-					Ustadha Qizra is born and brought up in India and has done Engineering with distinction from Osmania University. She has graduated in Alimah program from Nur ul ilm academy, Dubai.
+				Qizra Habeeb is born and brought up in India and has done Engineering with distinction from Osmania University. She has graduated in Alimah program from Nur ul ilm academy, Dubai.
 					Presently learning and sharing the knowledge of deen with the sisters only by the mercy of Allah ta’ala.
 				</p>
 				</div>
 				<div className="teacher">
 				<h2>Ustadha Sarah Sheikh</h2>
 				<p>
-					Ustadha Sarah is born in Mumbai brought up in Muscat (Oman), and has a bachelors degree in Interior Designing, with distinction from Annamalai University. She has graduated in Alimah program from Nur ul ilm Academy, Dubai. Presently learning and sharing the knowledge of deen with sisters, only by the Mercy of Allah (s.w.t).
+				Sarah Sheikh is born in Mumbai brought up in Muscat (Oman), and has a bachelors degree in Interior Designing, with distinction from Annamalai University. She has graduated in Alimah program from Nur ul ilm Academy, Dubai. Presently learning and sharing the knowledge of deen with sisters, only by the Mercy of Allah (s.w.t).
 				</p>
 				</div>
 				<div className="teacher">
 				<h2>Ustadha Sumaiah Aleem</h2>
-				<p>Ustadha Sumaiah was born and raised in India and has a bachelors degree in Computer Science Applications, with distinction, and a Masters degree in English literature from Osmania University. She has graduated in Alimah program from Nur ul Ilm Academy, Dubai. Presently learning and sharing the knowledge of deen with sisters, only by the Mercy of Allah (s.w.t).</p>
+				<p>Sumaiah Aleem was born and raised in India and has a bachelors degree in Computer Science Applications, with distinction, and a Masters degree in English literature from Osmania University. She has graduated in Alimah program from Nur ul Ilm Academy, Dubai. Presently learning and sharing the knowledge of deen with sisters, only by the Mercy of Allah (s.w.t).</p>
 				</div>
 				<div className="teacher">
 				<h2>Ustadha Sumayyah Shaikh</h2>
-				<p>Ustadha Sumayyah Shaikh is born and brought up in Muscat, Oman and has Bachelors degree in Psychology and Masters in English with distinction. She has graduated in Alimah program from Nur ul Ilm Academy, Dubai. Presently learning and sharing the knowledge of deen with sisters, only by the Mercy of Allah (s.w.t).</p>
+				<p>Sumayyah Shaikh is born and brought up in Muscat, Oman and has Bachelors degree in Psychology and Masters in English with distinction. She has graduated in Alimah program from Nur ul Ilm Academy, Dubai. Presently learning and sharing the knowledge of deen with sisters, only by the Mercy of Allah (s.w.t).</p>
 				</div>
 				<div className="teacher">
 				<h2>Ustadha Tamima Qader</h2>
-				<p>Ustadha Tamima Qader was born and brought up in Saudi Arabia, and has Bachelors degree in Computer Science Applications, with distinction, from Osmania University. She has graduated in Alimah program from Nur ul Ilm Academy, Dubai. Presently learning and sharing the knowledge of deen with sisters, only by the Mercy of Allah (s.w.t).</p>
+				<p>Tamima Qader was born and brought up in Saudi Arabia, and has Bachelors degree in Computer Science Applications, with distinction, from Osmania University. She has graduated in Alimah program from Nur ul Ilm Academy, Dubai. Presently learning and sharing the knowledge of deen with sisters, only by the Mercy of Allah (s.w.t).</p>
 			    </div>
 				<div className="teacher">
 				<h2>Ustadha Farheen Solanki</h2>
 				<p>Farheen Solanki is born and brought up in India, and has bachelors degree in Science, with distinction, from Bangalore University. She has graduated in Alimah program from Nur ul Ilm Academy, Dubai. Presently learning and sharing the knowledge of deen with sisters, only by the Mercy of Allah (s.w.t).</p>
 		    	</div>
+
+				<div className="teacher">
+				<h2>Ustadha Nusrath Inamul Huq</h2>
+				<p>Nusrath Inamul Huq is born and raised up in India. She has graduated in Alimah programme from Nur ul ilm, Dubai. Presently learning and sharing the knowledge with the sisters only by the mercy of Allah(s.w.t)</p>
+				</div>
+
+				<div className="teacher">
+				<h2>Ustadha Kashana Nidal</h2>
+				<p>Kashana Nidal is born in India and raised in Saudi Arabia. She has graduated in Alimah programme from Nur ul ilm, Dubai. Presently learning and sharing the knowledge of Quran with the sisters only by the mercy of Allah(s.w.t)</p>
+				</div>
+
+				<div className="teacher">
+				<h2>Ustadha Fathima Nasuha</h2>
+				<p>Fathima Nasuha is born in India and raised up in Dubai, UAE. She is a Hafiza and also has graduated in Alimah programme from Nur ul ilm, Dubai. Presently, learning and sharing the knowledge of Quran with the sisters only by the mercy of Allah (s.w.t).</p>
+				</div>
+
+				<div className="teacher">
+				<h2>Ustadha Tabassum Khan</h2>
+				<p>Tabassum Khan is born and brought up in India and has Bachelor degree in Chemistry and Master degree in Islamic studies from Mumbai university. She has graduated in Alimah program from Nur ul ilm Academy, Dubai (online). Presently learning and sharing the knowledge of deen with sisters, only by the Mercy of Allah (s.w.t).</p>
+				</div>
+
                 </section>
 
                 <aside id="events">
